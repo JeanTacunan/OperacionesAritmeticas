fd6d88357834c28633791c58c62e27b7ba8ab1ce
JeanTacunan <71549818@continental.edu.pe>
2024-09-02 18:52:15 -0500

    Proyecto de suma en POO

---
+++ OperacionesAritmeticas.py
@@ -0,0 +1,10 @@
+class OperacionesAritmeticas():
+    def IngresoNumeros(self):
+        numero1 = float(input("Ingrese sumando uno: "))
+        numero2 = float(input("Ingrese sumando dos: "))
+        return numero1, numero2
+
+
+    def suma(self, numero1, numero2):
+        return numero1 + numero2
+
